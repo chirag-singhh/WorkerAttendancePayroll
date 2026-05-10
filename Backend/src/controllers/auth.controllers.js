451a296a9896f91321df71404e2526549ceef6b8
Chirag <cs5764532@gmail.com>
2026-05-10 18:25:36 +0530

    updated

--- Backend/src/controllers/auth.controllers.js
+++ Backend/src/controllers/auth.controllers.js
@@ -46,8 +46,8 @@ export const registerUser = async (req, res) => {
     // Store Token in Cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -96,8 +96,8 @@ export const loginUser = async (req,res) => {
       // Store Token in Cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieparser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://worker-attendance-payroll.vercel.app/login"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: [
